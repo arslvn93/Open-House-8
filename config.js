fc29f97749236e53b2e0f69220cc0215fd62cab6
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-05-16 18:12:36 -0400

    Update Site

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
     "city": "Toronto",
     "cityStateZip": "Toronto, ON M4V 2H1",
     "heroSubtitle": "Fully Renovated Townhouse Loft with Massive Terrace, Sauna & Sunset Views. Register to receive the property website, floor plan, virtual tour, neighbourhood insights, and comparables.",
-    "backgroundImageUrl": "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80"
+    "backgroundImageUrl": "https://live-images.stratuscollab.com/_S64z0x9V3LJCJChzVkhf84mypXFrHSJYs_4YyMuG9w/rs:fill:1900:1900:0/g:no/cb:MjAyNS0wNS0xMlQwOTowODo0MC4wMDBa/aHR0cHM6Ly90cnJlYi1pbWFnZS5hbXByZS5jYS9kUXVLWG5NZzh5RXVqZTM1NXJWRjFpNmV2eVl2aHJlUXZDYlQ3Z04wdnlnL3JzOmZpdC93OjE5MDAvaDoxOTAwL2c6Y2Uvd206LjU6c286MDo1MDouNC93bXNoOjEwL3dtdDpQSE53WVc0Z1ptOXlaV2R5YjNWdVpEMG5kMmhwZEdVbklHWnZiblE5SnpZNEp6NVNSVUZNSUVKU1QwdEZVaUJQVGxSQlVrbFBJRXhVUkM0c0lFSnliMnRsY21GblpUd3ZjM0JoYmo0L0wzUnljbVZpTDJ4cGMzUnBibWR6THpReEx6RTVMelU0THpjMUwzQXZNbVUxTWpZMFpUTXROREptWkMwME9UTTBMVGt4TnpjdE9USTBaVGs0TURjM1lXRTJMbXB3WncuanBn.jpg"
   },
   "realtor": {
     "fullName": "Ryan Wykes",
@@ -26,16 +26,38 @@ const config = {
   },
   "openHouse": {
     "bundleItems": [
-      { "icon": "fas fa-ruler-combined", "text": "Architectural Floor Plan: https://app.realmmlp.ca/shared/XlRq9W88bVcjmoM85rqe/eP5L3rZYmMUyjk5GgEjaia3A24la7jtykbrq3V2Qirj6l9WRW4Hw8VMPQXbwFzn9Aj2VeeiowmbWylrEsyzGOaXW5GcrnkwQwjnwh9LREjEgLEFRBEz4KknwfgqV9eA2MYfeke3Bj" },
-      { "icon": "fas fa-star", "text": "Benefit: Massive Terrace w Sauna, Fire Place, Sunset Views and more" },
-      { "icon": "fas fa-check-circle", "text": "Benefit: Unique Modern Sun filled Loft meets Character Home" },
-      { "icon": "fas fa-key", "text": "Benefit: Best Neighbourhood in The City. Walk to Yorkville, Summerhill & Foresthill" },
-      { "icon": "fas fa-house", "text": "Key Feature: Fully renovated Townhouse Loft (20 ft. Ceilings in Living/Kitchen) with Victoria Charm" },
-      { "icon": "fas fa-map-location-dot", "text": "Neighbourhood: Walking distance to Best Shops, Restaurants, Schools and Public Transit" },
-      { "icon": "fas fa-link", "text": "Property Website: https://media.tre.media/listings/0196b059-13f0-7271-9320-b4e0e0c4b410/download-center#property_websites" },
-      { "icon": "fas fa-film", "text": "Virtual Tour: https://youriguide.com/th4_300_avenue_rd_toronto_on" },
-      { "icon": "fas fa-clone", "text": "Comparables & Recent Sales: https://app.realmmlp.ca/shared/XlRq9W88bVcjmoM85rqe/w23m6VlkLwh9r4jVZnr6s5yGeP85XQhN8LMaVJnYtwrMjzlElYuV5lODgrj9S3qnJ9jVyPiX1bPPYYooHO4VXjLeqgHr4ogOpAweuGB6gKgJBgtY4QlalX4lFXRVonmrQQHBqz2y7B4Acnnw6518g" },
-      { "icon": "fas fa-link", "text": "Property Listing: https://app.realmmlp.ca/shared/XlRq9W88bVcjmoM85rqe/6Xqwo6M5V3Umlj2nXNlDiywOe7PymASYRyOgXwopfEebMDKWKOs8k6jXrYZ8SLKZgADOVVfErg1aPm5qiDGOMZPW9OIMLpDrDELDI28YEeEN8EUqaZgK5bYVi6Dp3RPaGyIYqYeE9" }
+      {
+        "icon": "fas fa-ruler-combined",
+        "text": "Architectural Floor Plan: https://app.realmmlp.ca/shared/XlRq9W88bVcjmoM85rqe/eP5L3rZYmMUyjk5GgEjaia3A24la7jtykbrq3V2Qirj6l9WRW4Hw8VMPQXbwFzn9Aj2VeeiowmbWylrEsyzGOaXW5GcrnkwQwjnwh9LREjEgLEFRBEz4KknwfgqV9eA2MYfeke3Bj"
+      },
+      {
+        "icon": "fas fa-star",
+        "text": "Benefit: Massive Terrace w Sauna, Fire Place, Sunset Views and more"
+      },
+      {
+        "icon": "fas fa-check-circle",
+        "text": "Benefit: Unique Modern Sun filled Loft meets Character Home"
+      },
+      {
+        "icon": "fas fa-key",
+        "text": "Benefit: Best Neighbourhood in The City. Walk to Yorkville, Summerhill & Foresthill"
+      },
+      {
+        "icon": "fas fa-house",
+        "text": "Key Feature: Fully renovated Townhouse Loft (20 ft. Ceilings in Living/Kitchen) with Victoria Charm"
+      },
+      {
+        "icon": "fas fa-map-location-dot",
+        "text": "Neighbourhood: Walking distance to Best Shops, Restaurants, Schools and Public Transit"
+      },
+      {
+        "icon": "fas fa-link",
+        "text": "Property Website: https://media.tre.media/listings/0196b059-13f0-7271-9320-b4e0e0c4b410/download-center#property_websites"
+      },
+      {
+        "icon": "fas fa-film",
+        "text": "Virtual Tour: https://youriguide.com/th4_300_avenue_rd_toronto_on"
+      }
     ]
   },
   "meta": {
@@ -46,16 +68,31 @@ const config = {
   "settings": {
     "adminWebhookUrl": "https://n8n.salesgenius.co/webhook/openhouseupdate",
     "visitorWebhookUrl": "https://n8n.salesgenius.co/webhook/openhouse",
-    "confettiColors": ["#6187a7", "#f0e9d6", "#ffffff", "#83a363", "#d9ac68"]
+    "confettiColors": [
+      "#6187a7",
+      "#f0e9d6",
+      "#ffffff",
+      "#83a363",
+      "#d9ac68"
+    ]
   },
   "modalQuestions": [
     {
       "id": "agent_status",
       "questionText": "Are you currently exploring homes with a real estate agent?",
       "options": [
-        { "value": "committed", "text": "Yes, I'm committed to an agent." },
-        { "value": "not_committed", "text": "No, I'm not committed to an agent." },
-        { "value": "gathering_info", "text": "Just gathering information at this stage." }
+        {
+          "value": "committed",
+          "text": "Yes, I'm committed to an agent."
+        },
+        {
+          "value": "not_committed",
+          "text": "No, I'm not committed to an agent."
+        },
+        {
+          "value": "gathering_info",
+          "text": "Just gathering information at this stage."
+        }
       ]
     }
   ],
@@ -66,4 +103,4 @@ const config = {
     "netlifyUrl": "http://Open-House-8.netlify.app",
     "netlifyId": "ba52597e-51ba-4cac-ba69-ce50acecf966"
   }
-}
+};
